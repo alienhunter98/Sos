fa878f688d9a94bc40880a736f630c609fa5fca6
alienhunter98 <alienhunter@bk.ru>
2020-01-21 17:32:45 +0500

    Sos kids v.1

---
+++ app/js/main.js
@@ -0,0 +1,232 @@
+
+$(document).ready(function(){
+
+	$('#search').change(function(){
+    if($(this).prop('checked')) $('[type="search"]').trigger('focus')
+	})
+	$('[type="search"]').focusout(function(){
+	    $('#search').prop('checked','')
+	})
+	/*FANCYBOX*/
+	if ($("[data-fancybox]").length != 0)
+		$("[data-fancybox]").fancybox({
+			afterShow: function(instance, current) {},
+			animationEffect : "zoom",
+			animationDuration : 800,
+			thumbs : {
+				autoStart   : true
+			},
+			touch : false,
+			transitionDuration : 366,
+			transitionEffect: "zoom-in-out"
+		});
+
+	/*Подключение owl carousel*/
+	$('.main-carousel').owlCarousel({
+	    loop: true, // Зациклирование
+	    margin: 0, // Отступы
+	    nav: true, // Навигация
+	    dots: true, // Точки
+	    dotsEach:true,
+
+	    navText: [
+	    	'<svg class="flickity-button-icon btn-left" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>',
+	    	'<svg class="flickity-button-icon btn-right" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>'
+	    	],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{ 
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	});
+	$('.waw-carousel').owlCarousel({
+	    loop: true, // Зациклирование
+	    margin: 0, // Отступы
+	    nav: true, // Навигация
+	    dots: false, // Точки
+	    dotsEach:true,
+	    
+	    navText: [
+	    	'<svg class="flickity-button-icon btn-left" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>',
+	    	'<svg class="flickity-button-icon btn-right" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>'
+	    	],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{ 
+	            items:2
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	});
+	
+	$('.charity-carousel').owlCarousel({
+	    loop: true, // Зациклирование
+	    margin: 30, // Отступы
+	    nav: false, // Навигация
+	    dots: true, // Точки
+	    dotsEach:false,
+	    
+	    navText: [
+	    	'<svg class="flickity-button-icon btn-left" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>',
+	    	'<svg class="flickity-button-icon btn-right" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>'
+	    	],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{ 
+	            items:2
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	});
+	$('.partners-carousel').owlCarousel({
+	    loop: true, // Зациклирование
+	    margin: 20, // Отступы
+	    nav: true, // Навигация
+	    dots: false, // Точки
+	    dotsEach:false,
+	    
+	    navText: [
+	    	'<svg class="flickity-button-icon btn-left" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>',
+	    	'<svg class="flickity-button-icon btn-right" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>'
+	    	],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{ 
+	            items:2
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	});
+	$('.idea-carousel').owlCarousel({
+	    loop: false, // Зациклирование
+	    margin: 30, // Отступы
+	    nav: false, // Навигация
+	    dots: true, // Точки
+	    dotsEach:true,
+	    startPosition:1,
+	    navText: [
+	    	'<svg class="flickity-button-icon btn-left" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>',
+	    	'<svg class="flickity-button-icon btn-right" viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" class="arrow"></path></svg>'
+	    	],
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{ 
+	            items:1
+	        },
+	        1000:{
+	            items:2
+	        }
+	    }
+	});
+	$(".js-select").select2({
+    tags: false,
+    allowClear: false
+  });
+	
+ 	$("[data-text-limit]").map(function( i, el ){
+      el = $(el);
+      var text = el.text();
+      var textLimit = el.attr("data-text-limit");
+
+      if( text.length > textLimit*1 ){
+        text = text.substring(0, textLimit )
+        el.text( text+ " ..." );
+      }
+    });
+ 	// Прибавление-убавление значении
+    (function(){
+      var form = $("[data-counter]") || null;;
+      if( !form )
+        return;
+      var cntfactor = form.attr("data-counter")*1;
+
+      $(document).on("click", "[data-counter-btn]", function(){
+        var cntVal;
+        var cntInput = $(this).closest( form ).find("[data-counter-input]");
+        
+        cntVal = (cntInput.val()*1);
+
+        if( $(this).hasClass("plus") )
+          cntVal = cntVal + cntfactor;
+        if( $(this).hasClass("minus") & cntVal > 0 )
+          cntVal = cntVal - cntfactor;
+        if( isNaN( cntVal ) || cntVal < 0) cntVal = 0;
+        cntInput.val( cntVal ).attr("value", cntVal)
+      })
+      $(".cnt-input").on( "keypress", function(e){
+        //console.log(this, e);
+      } )
+
+    })();
+
+ //закрывание предыдущих табов 
+	var tabContent = $('header .tab-content');
+	tabContent.find('[data-toggle="collapse"]').on("click", function(){
+		tabContent.find(".collapse").collapse("hide");
+	})
+
+
+	//SCROLL
+    var minMenu = $(".header-scroll") || null;
+    var headerRange = false;
+    var staffProgressStatus = false;
+    $(window).on("scroll", function(e) {
+
+      //Адаптация хедера при скролинге
+      if ($(window).scrollTop() > 90 && headerRange == false) {
+
+        headerRange = true;
+        if (minMenu) minMenu.addClass("scrolled");
+
+      } else if ($(window).scrollTop() < 90 && headerRange == true) {
+        headerRange = !true;
+        if (minMenu) minMenu.removeClass("scrolled");
+      } //.originalEvent.wheelDelta
+
+
+    });
+    $(window).trigger("scroll");
+
+
+
+// Проверка на заполненность
+    $(".form-control").on("keyup", function(){
+      var that = $(this);
+      if( that.val().length )
+        that.addClass("filled")
+      else
+        that.removeClass("filled");
+    })
+
+
+});
+
+
+
+
+
+
+
+
+
+console.log("end script");
